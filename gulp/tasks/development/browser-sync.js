b2297da3329a036191d0692e33b425ab2ab2c8ca
chandler9wilson <chandler9wilson@gmail.com>
2016-08-11 20:33:33 -0500

    added majority of styling maps still not working

--- gulp/tasks/development/browser-sync.js
+++ gulp/tasks/development/browser-sync.js
@@ -5,14 +5,8 @@ var dev = browsersync.create('dev');
 
 gulp.task('browsersync', [], function() {
     dev.init({
-        server: {
-            baseDir: ['./src']
-        },
-        port: 9999,
-        files: [
-            'src/css/*.css',
-            'src/js/*.js',
-            'src/images/**'
-        ]
+        server: ['./src/', './node_modules']
     });
+
+    gulp.watch('src/*.html').on('change', browsersync.reload);
 });
---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+var map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 40.7413549, lng: -73.99802439999996},
+        zoom: 13
+    });
+/*var tribeca = {lat: 40.719526, lng: -74.0089934};
+var marker = new google.maps.Marker({
+    position: tribeca,
+    map: map,
+    title: 'tribeca'
+});
+var infowindow= new google.maps.InfoWindow({
+    content: 'la;sdkfjl;asdfj'
+});
+marker.addListener('click', function() {
+    infowindow.open(map, marker);
+});*/
+}
