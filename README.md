76baa444cbf023c004351865e4d47bf56f7e8c37
chandler9wilson <chandler9wilson@gmail.com>
2016-10-11 20:37:06 -0500

    location data model simplified ui now updating correctly on locationArray change

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-# Questions:
+# Getting Started:
+
+1. Get the project and navigate to the project directory 
+2. `$ npm install` will install all of the dev requirments via npm
+3. `$ gulp` will open a test server and auto refresh after changes to `/src/**` as well as run `.js` and `.css` through a lint and write the results to console
+
+## About:
+
+This is a project for the Udactiy Front End Nanodegree.
+
+## Questions:
 
 1. Why does pushing to the underlying array `areaArray().push()` work, but pushing using the knockout method `areaArray.push()` does not work?
 The main problem is that pushing to the underlying array does not trigger the view update.
--- src/js/main.js
+++ src/js/main.js
@@ -9,25 +9,25 @@ function initMap() {
     var lat = mapArea.lat;
     var lng = mapArea.lng;
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat, lng},
+        center: { lat, lng },
         zoom: 14
-        //todo change control defaults to prevent interference with new location button
+            //todo change control defaults to prevent interference with new location button
             //https://developers.google.com/maps/documentation/javascript/controls#Adding_Controls_to_the_Map
-        //todo change the map space to change on rezise so that 15% of map is not off screen on desktop
+            //todo change the map space to change on rezise so that 15% of map is not off screen on desktop
     });
-    var tribeca = {lat: 40.719526, lng: -74.0089934};
+    var tribeca = { lat: 40.719526, lng: -74.0089934 };
     var marker = new google.maps.Marker({
         position: tribeca,
         map: map,
         title: 'tribeca'
     });
-    var infowindow= new google.maps.InfoWindow({
+    var infowindow = new google.maps.InfoWindow({
         content: 'la;sdkfjl;asdfj'
     });
     marker.addListener('click', function() {
         infowindow.open(map, marker);
     });
-}   
+}
 
 //initial map area used in initMap
 var mapArea = {
@@ -35,13 +35,35 @@ var mapArea = {
     lng: -97.74473390000001
 };
 
+//Main view model
 var ViewModel = function() {
+    var self = this;
+    //todo add current object and state machine
 
+    //todo implement areas
+    /*var allAreas = ko.observableArray([
+        {'default': new Area()}
+    ]);*/
+
+    self.locationArray = ko.observableArray([]);
+
+    //function called when a location is being added
+    self.newLocation = function(address, nickname) {
+        var obj = {};
+        obj['nickname'] = nickname;
+        obj['placeInfo'] = new Location(address);
+
+        //push the newLocation to locationArray 
+        self.locationArray.push(obj);
+    };
+};
+
+//location class used within viewmodel
+var Location = function(address) {
+    //stores the google maps json info
+    this.address = address;
 };
 
 var vm = new ViewModel();
 
 ko.applyBindings(vm);
-
-
-
