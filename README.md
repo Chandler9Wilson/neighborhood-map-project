eefcc93a5d15c3a7bee6d23aab026676a01d8cb4
chandler9wilson <chandler9wilson@gmail.com>
2017-03-09 14:55:29 -0600

    updated to 5 locations, readme updated with foursquare attribution

--- README.md
+++ README.md
@@ -8,12 +8,11 @@
 
 This is a project for the Udactiy Front End Nanodegree.
 
+Checkins provided by Foursquare
+
 ## Questions:
 
 1. How do i not reveal the client_secret when the app is client side only atm?
-2. Places Autocomplete runs but is not displaying suggestions I found a [thread with a similar issue but in a bootstrap modal](http://stackoverflow.com/questions/10957781/google-maps-autocomplete-result-in-bootstrap-modal-dialog)
-I have tried several of the suggested solutions but nothing has worked so far not sure what I am missing.
-3. Part of the map is pushed off the page when in fullscreen desktop. I have not been able to figure out if this is an issue with mdl or how I am declaring the map properties in css
 
 ## Useful resources
 
--- src/js/defaultData.js
+++ src/js/defaultData.js
@@ -1,33 +1,53 @@
 var defaultData = {
     "locations": [{
-            "nickname": "Jimmy Johns",
-            "formalName": "Jimmy Johns",
-            "address": "601 W Martin Luther King Jr Blvd, Austin, TX 78701, USA",
-            "query": "Jimmy Johns, 601 W Martin Luther King Jr Blvd, Austin, TX 78701, USA",
-            "ll": {
-                "lat": 30.2821355,
-                "lng": -97.7440055
-            }
-        },
-        {
-            "nickname": "Art Museum",
-            "formalName": "Jack S. Blanton Museum of Art",
-            "address": "200 E Martin Luther King Jr Blvd, Austin, TX 78701, USA",
-            "query": "Jack S. Blanton Museum of Art, 200 E Martin Luther King Jr Blvd, Austin, TX 78701, USA",
-            "ll": {
-                "lat": 30.2809836,
-                "lng": -97.73741129999999
-            }
-        },
-        {
-            "nickname": "Pease Park",
-            "formalName": "pease district park",
-            "address": "1100 Kingsbury St, Austin, TX 78703, USA",
-            "query": "pease district park, 1100 Kingsbury St, Austin, TX 78703, USA",
-            "ll": {
-                "lat": 30.2813557,
-                "lng": -97.7523046
-            }
+        "nickname": "Jimmy Johns",
+        "formalName": "Jimmy Johns",
+        "address": "601 W Martin Luther King Jr Blvd, Austin, TX 78701, USA",
+        "query": "Jimmy Johns, 601 W Martin Luther King Jr Blvd, Austin, TX 78701, USA",
+        "ll": {
+            "lat": 30.2821355,
+            "lng": -97.7440055
         }
+    },
+    {
+        "nickname": "Art Museum",
+        "formalName": "Jack S. Blanton Museum of Art",
+        "address": "200 E Martin Luther King Jr Blvd, Austin, TX 78701, USA",
+        "query": "Jack S. Blanton Museum of Art, 200 E Martin Luther King Jr Blvd, Austin, TX 78701, USA",
+        "ll": {
+            "lat": 30.2809836,
+            "lng": -97.73741129999999
+        }
+    },
+    {
+        "nickname": "Pease Park",
+        "formalName": "pease district park",
+        "address": "1100 Kingsbury St, Austin, TX 78703, USA",
+        "query": "pease district park, 1100 Kingsbury St, Austin, TX 78703, USA",
+        "ll": {
+            "lat": 30.2813557,
+            "lng": -97.7523046
+        }
+    },
+    {
+        "nickname": "Tiff's Treats",
+        "formalName": "Tiff's Treats",
+        "address": "1806 Nueces St, Austin, TX 78701",
+        "query": "Tiff's Treats, 1806 Nueces St, Austin, TX 78701",
+        "ll": {
+            "lat": 30.281872,
+            "lng": -97.744117
+        }
+    },
+    {
+        "nickname": "Capital Factory",
+        "formalName": "Capital Factory",
+        "address": "701 Brazos St, Austin, TX 78701",
+        "query": "Capital Factory, 701 Brazos St, Austin, TX 78701",
+        "ll": {
+            "lat": 30.268876,
+            "lng": -97.740564
+        }
+    }
     ]
 };
